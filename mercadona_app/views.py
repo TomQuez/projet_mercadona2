fed921e333a7954a2302f87582a6639601fc4701
TomQuez <tom.quez@live.fr>
2023-10-30 15:02:22 +0100

    ajout pagination index.html

--- mercadona_app/views.py
+++ mercadona_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from mercadona_app.models import Product,Promotion,Categorie
 from django.views import generic
+from django.core.paginator import Paginator
 
 
 
@@ -15,9 +16,13 @@ def index(request):
         product_list=Product.objects.filter(category__name=selected_category)
     else:
         product_list=Product.objects.all()
+        
+    paginator=Paginator(product_list,2)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
     
     context={
-        'product_list':product_list,
+        'product_list':page_obj,
         'promotion_list':promotion_list,
         'categories':categories,
         'selected_category':selected_category
@@ -34,15 +39,18 @@ def product_list(request):
     
     categories=Categorie.objects.all()
     selected_category=request.GET.get('category',None)
+    
     if selected_category:
         products=Product.objects.filter(category__name=selected_category)
     else:
         products=Product.objects.all()
 
-        
+    paginator=Paginator(products,1)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
     context={
         'categories':categories,
         'selected_category':selected_category,
-        'products':products,
+        'products':page_obj,
     }
     return render(request,'categories.html',context=context)
--- projet_mercadona/settings.py
+++ projet_mercadona/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR=['static']
+# STATICFILES_DIR=['static']
 MEDIA_URL='/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'/')
 
