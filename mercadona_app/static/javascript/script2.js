c8b82705dcb7f2b26753bec3e051a8ff822d4bb7
TomQuez <tom.quez@live.fr>
2023-11-06 15:18:49 +0100

    ajout fonctionnalité filtre par catégorie

--- mercadona_app/static/javascript/script2.js
+++ mercadona_app/static/javascript/script2.js
@@ -1,34 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const productList = document.getElementById("product-list");
+  const categoryDropdown = document.getElementById("category");
+
+  let allProducts = []; // Pour conserver tous les produits d'origine.
+
   fetch("get_catalog_data/")
     .then((response) => response.json())
     .then((data) => {
-      const products = data.products;
-      const productList = document.getElementById("product-list");
-      const categoryDropdown = document.getElementById("category");
+      allProducts = data.products; // Conservez tous les produits d'origine.
+
+      // Remplissez le menu déroulant des catégories.
       const uniqueCategories = [
-        ...new Set(products.map((product) => product.category)),
+        ...new Set(allProducts.map((product) => product.category)),
       ];
-      console.log(uniqueCategories);
       uniqueCategories.forEach((category) => {
         const option = document.createElement("option");
         option.value = category;
         option.textContent = category;
         categoryDropdown.appendChild(option);
       });
+
+      // Affichez tous les produits au chargement initial.
+      displayProducts(allProducts);
+
+      // Ajoutez un gestionnaire d'événements pour le changement de catégorie.
       categoryDropdown.addEventListener("change", function () {
         const selectedCategory = categoryDropdown.value;
         const filteredProducts = filterProductsByCategory(
-          products,
+          allProducts,
           selectedCategory
         );
+        displayProducts(filteredProducts);
       });
+    })
+    .catch((error) => {
+      console.error(
+        "Une erreur s'est produite lors de la récupération des données :",
+        error
+      );
+    });
 
-      products.forEach((product) => {
-        const productDiv = document.createElement("div");
-        productDiv.classList.add("row");
+  // Fonction pour afficher les produits dans le catalogue.
+  function displayProducts(products) {
+    productList.innerHTML = ""; // Effacez le contenu précédent.
 
-        if (product.promotion && product.promotion.status == true) {
-          productDiv.innerHTML = `
+    products.forEach((product) => {
+      const productDiv = document.createElement("div");
+
+      productDiv.classList.add("row");
+
+      if (product.promotion && product.promotion.status == true) {
+        productDiv.innerHTML = `
               <div class="card m-auto mt-2" style="width:18rem;">
               <img class="card-img-top mt-3" src="${product.image}" alt="Card image cap">
               <div class="card-body">
@@ -43,8 +65,8 @@ document.addEventListener("DOMContentLoaded", function () {
               </div>
                            
           `;
-        } else {
-          productDiv.innerHTML = `
+      } else {
+        productDiv.innerHTML = `
         <div class="card m-auto mt-2" style="width:18rem;">
         <img class="card-img-top mt-3" src="${product.image}" alt="Card image cap">
         <div class="card-body">
@@ -59,15 +81,18 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
               
           `;
-        }
+      }
 
-        productList.appendChild(productDiv);
-      });
-    })
-    .catch((error) => {
-      console.error(
-        "Une erreur s'est produite lors de la récupération des données :",
-        error
-      );
+      productList.appendChild(productDiv);
     });
+  }
+
+  // Fonction pour filtrer les produits par catégorie.
+  function filterProductsByCategory(products, category) {
+    if (!category) {
+      return products; // Retournez tous les produits si aucune catégorie n'est sélectionnée.
+    }
+
+    return products.filter((product) => product.category === category);
+  }
 });
